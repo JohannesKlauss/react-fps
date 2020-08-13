79fbcc8dec64c201e592ced4b0b848939cec8e5b
Johannes Klauss <klauss.johannes@gmail.com>
2020-08-13 11:13:21 +0200

    Init commit

---
+++ src/hello-world.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import HelloWorld from "./hello-world";
+
+export default {
+  title: "Hello World",
+  component: HelloWorld,
+};
+
+export const Default = (): React.ReactNode => <HelloWorld />;
---
+++ src/hello-world.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HelloWorld: React.FC = () => {
+  return <div>Hello World</div>;
+};
+
+export default HelloWorld;
